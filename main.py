0ccb7e3bdea55d06398c0281496490b7ec2c8d63
zNyller <edu.sampaio1777@gmail.com>
2024-08-14 15:39:16 -0300

    Otimizações e algumas alterações.
    Modularização com adição dos arquivos messages.py (para armazenar mensagens exibidas para o usuário) e utils.py (para armazenar funções uteis ao código principal).
    Refatoramento de código para maior coesão e eficiência.

--- main.py
+++ main.py
@@ -1,37 +1,54 @@
+import logging
+import messages
+import utils
+
 class Aplicativo:
     """Clase principal. Cria, Gerencia e Exibe as tarefas do usuário."""
+
+    DONE = 'Concluída'
+    UNDONE = 'Não concluída'
+
     def __init__(self) -> None:
         self.tasks = []
+        self.options = {
+            1: 'Concluir', 2: 'Desmarcar conclusão', 3: 'Editar', 4: 'Deletar', 5: 'Sair'
+        }
 
     def menu(self):
         """Exibe mensagem de boas vindas e pede a adição de uma primeira tarefa."""
-        print('Bem-vindo à sua nova jornada!\n\nPara começar, crie uma tarefa!')
+        print(messages.WELCOME_MSG)
         tarefa1 = input('Nome da sua primeira tarefa: ')
-        self.tasks.append(tarefa1)
-        print('Sua primeira tarefa foi criada com sucesso!')
+        self.tasks.append({'nome': tarefa1, 'status': self.UNDONE})
+        print(messages.TASK_CREATED)
 
     def task_loop(self):
         """Gerencia um loop para adição de novas tarefas."""
-        print('\nPara adicionar outra tarefa, insira o nome. Se deseja prosseguir, digite "S".')
+        print(messages.TASK_LOOP_MSG)
         while True:
-            task = input('Nome da tarefa: ').capitalize()
-            if task == 'S':
+            task_name = input('Nome da tarefa: ').capitalize()
+            if task_name == 'S':
                 break
-            self.tasks.append(task)
-            print('Tarefa adicionada!')
+            self.add_task(task_name)
+
+    def add_task(self, name):
+        """Cria um dicionário com 'nome' e 'status' para a nova tarefa e o adiciona na lista."""
+        new_task = {'nome': name, 'status': self.UNDONE}
+        self.tasks.append(new_task)
+        print(messages.TASK_CREATED)
 
     def show_tasks(self):
         """Exibe a lista de tarefas do usuário."""
-        print('Suas tarefas:')
+        print(messages.YOUR_TASKS)
         for index, task in enumerate(self.tasks, start=1):
-            print(f'[{index}] - {task}')
+            print(f'[{index}] - {task["nome"]} | {task["status"]}')
 
     def console(self):
         """Exibe e trata as opções do usuário para gerenciar as tarefas."""
-        print('\nSelecione uma das tarefas para gerenciar.')
+        print(messages.SELECT_TASK)
         task_number = self._get_task_number()
-        print('Opções: [1] Marcar como concluída | [2] Não concluída | [3] Editar tarefa | [4] Deletar tarefa')
-        user_option = input('Escolha uma das opções: ')
+        for key, value in self.options.items():
+            print(f'[{key}] - {value}')
+        user_option = utils.convert_to_int(input_msg='Escolha uma das opções: ')
         self._handle_options(task_number, user_option)
 
     def _get_task_number(self):
@@ -40,20 +57,35 @@ class Aplicativo:
             task_number = input('Número da tarefa: ')
             try:
                 int_task_number = int(task_number) - 1
-                print(f'Tarefa {self.tasks[int_task_number]} selecionada!')
+                print(f'\nTarefa {self.tasks[int_task_number]["nome"]} selecionada!')
                 return int_task_number
             except IndexError:
-                print('Número da tarefa não encontrado! Tente novamente.')
+                logging.error('Número da tarefa não encontrado! Tente novamente.')
             except ValueError:
-                print('Digite um número válido!')
+                logging.error('Digite um número válido!')
             except Exception as e:
-                print('Erro!', e)
+                logging.error('Erro!', e)
 
     def _handle_options(self, task, option):
         """Lida com a opção selecionada pelo usuário."""
+        current_task = self.tasks[task]
         if option == 1:
-            # Implementar
+            current_task['status'] = self.DONE
+            print(messages.MARKED_AS_DONE)
+        elif option == 2:
+            if current_task['status'] != self.UNDONE:
+                current_task['status'] = self.UNDONE
+                print(messages.MARKED_AS_UNDONE)
+            else:
+                print(messages.ALREADY_UNDONE)
+        elif option == 3:
             ...
+        elif option == 4:
+            ...
+        elif option == 5:
+            print('Até a próxima!')
+        else:
+            print(messages.OPTION_OUT_OF_RANGE)
 
 
 def main():
@@ -62,6 +94,7 @@ def main():
     app.task_loop()
     app.show_tasks()
     app.console()
+    app.show_tasks()
 
 
 if __name__ == '__main__':
